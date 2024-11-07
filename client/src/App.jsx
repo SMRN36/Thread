ff81065713662b9bcb3e9d79f07c227bc47781bf
Simran Bhandari <simranbhandari036@gmail.com>
2024-11-07 17:26:27 +0530

    Header and Routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 import Loading from "./components/common/Loading";
-
+import Header from "./components/common/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Protected/Home";
+import Search from "./pages/Protected/Search";
 
 const App = () =>{
   return(
     <>
-      <Loading />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/search" element={<Search />} />
+          <Route exact path="/user" element={<h1>User Profile</h1>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
